4f3feaa87d1ba12a3ac0686f069c37c4f5da829c
IgorMichael <igormichael120@gmail.com>
2022-02-18 01:38:21 -0300

    Progress bar added and .env.example created

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import { Header } from "../components/Header";
 import { SessionProvider } from "next-auth/react";
 
 import "../styles/global.scss";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <NextNProgress
+        color="#eba417"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Header />
       <Component {...pageProps} />
     </SessionProvider>
